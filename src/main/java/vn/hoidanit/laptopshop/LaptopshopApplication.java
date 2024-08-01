7a75b8e6742a9ba8eaa7bdccbde4ba1cd4e0bdb0
Hoang. Vu Quoc - CMC Global DJ2 <vqhoang@cmcglobal.vn>
2024-08-01 11:07:59 +0700

    mvc

--- src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
+++ src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(exclude =
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
 public class LaptopshopApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LaptopshopApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(LaptopshopApplication.class, args);
 
 //        List bean:
 //        ApplicationContext hoidanit = SpringApplication.run(LaptopshopApplication.class, args);
@@ -16,6 +18,6 @@ public class LaptopshopApplication {
 //                String s : hoidanit.getBeanDefinitionNames()) {
 //            System.out.println(s);
 //        }
-	}
+    }
 
 }
